e4b30bcb51126bd38493cb96760db87c79b83fc0
truda8 <chenhaikun8@gmail.com>
2021-10-27 11:00:44 +0800

    first submit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module socks5proxy
+
+go 1.16
+
+require (
+	github.com/armon/go-socks5 v0.0.0-20160902184237-e75332964ef5 // indirect
+	golang.org/x/net v0.0.0-20211020060615-d418f374d309 // indirect
+)
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/armon/go-socks5"
+)
+
+type Config struct {
+	host string
+	port string
+	user string
+	pass string
+}
+
+func main() {
+	config := getArgs()
+
+	// Username and password auth config
+	cred := socks5.StaticCredentials{
+		config.user: config.pass,
+	}
+	auth := socks5.UserPassAuthenticator{Credentials: cred}
+
+	// Create a SOCKS5 server
+	conf := &socks5.Config{AuthMethods: []socks5.Authenticator{auth}}
+	server, err := socks5.New(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create SOCKS5 proxy on localhost port 8000
+	if err := server.ListenAndServe("tcp", config.host+":"+config.port); err != nil {
+		panic(err)
+	}
+}
+
+func getArgs() Config {
+	var config Config
+	flag.StringVar(&config.host, "host", "127.0.0.1", "Listen host.")
+	flag.StringVar(&config.port, "port", "1080", "Listen port.")
+	flag.StringVar(&config.user, "user", "admin", "Socks5 username.")
+	flag.StringVar(&config.pass, "pass", "admin", "Socks5 password.")
+	flag.Parse()
+	return config
+}
